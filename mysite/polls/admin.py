b485338979669bebc165aef50b995912f4064e3c
cardosocaro <carocardoso.m14@gmail.com>
2025-04-12 20:58:08 -0300

    Parte 7.3 Personalice la lista de cambios del sitio administrativo¶

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,6 +4,9 @@ from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ["question_text"]
+    list_filter = ["pub_date"]
+    list_display = ("question_text", "pub_date", "was_published_recently")
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),

5bcb5e4814e6e8e06158de59da1568a2ced4b8bb
cardosocaro <carocardoso.m14@gmail.com>
2025-04-12 20:52:54 -0300

    Parte 7.2 agregando objetos relacionados

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -8,6 +8,18 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)

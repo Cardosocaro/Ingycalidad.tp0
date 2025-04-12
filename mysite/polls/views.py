f0aa002a1fda24aaa9747f69f9b6a77e19902a3a
cardosocaro <carocardoso.m14@gmail.com>
2025-04-12 18:30:33 -0300

    [Parte 3.2] Escriba vistas que realmente hagan algo

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
 
 def index(request):
@@ -15,3 +17,18 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+    from django.http import HttpResponse
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
